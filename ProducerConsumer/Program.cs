3f57747be830b64ba1e2b19cb81eec76700efd34
MuuhMuuh <Jesper_ant@hotmail.com>
2014-09-27 18:37:15 +0200

    Fik rettet en fejl.
    Tidligere havde jeg glemt at tilføje en consumer, der tog items fra buffer2, så når buffer2 blev fyldt, låste programmet. MiddleMan er løst.

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -12,20 +12,21 @@ namespace ProducerConsumer
         static void Main(string[] args)
         {
             BoundedBuffer buffer = new BoundedBuffer(4);
-            BoundedBuffer buffer2 = new BoundedBuffer(3); //Brugt til MiddleMan
+            BoundedBuffer buffer2 = new BoundedBuffer(4); //Brugt til MiddleMan
 
             Producer prod = new Producer(buffer, 15);
             //Consumer cons = new Consumer(buffer);
             //Consumer cons2 = new Consumer(buffer);
             MiddleMan mm = new MiddleMan(buffer, buffer2); //Tager fra buffer og indsætter i buffer2
-
+            Consumer consMm = new Consumer(buffer2);
 
             //Brug Parallel.Invoke til at køre dem
            Parallel.Invoke(
                 () => prod.Run(), 
                 //() => cons.Run(),
                 //() => cons2.Run()
-                () => mm.Run()
+                () => mm.Run(),
+                () => consMm.Run()
                 );
             Console.WriteLine("Done!");
            Console.ReadLine();
