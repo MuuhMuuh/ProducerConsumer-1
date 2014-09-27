1f01d07fb1c6323d73d99bd0168bcf7580b35572
MuuhMuuh <Jesper_ant@hotmail.com>
2014-09-27 18:55:56 +0200

    Rettet Program.cs til, så det er pænere sat sammen med de forskellige opgaver udkommenteret.
    Har lavet Duplicator.cs, og tilføjet til Program.cs så det er den der er aktiv.

---
+++ ProducerConsumer/Duplicator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Duplicator
+    {
+        private BoundedBuffer _bufferIn;
+        private BoundedBuffer _bufferOut1;
+        private BoundedBuffer _bufferOut2;
+
+        public Duplicator(BoundedBuffer bufferIn, BoundedBuffer bufferOut1, BoundedBuffer bufferOut2)
+        {
+            _bufferIn = bufferIn;
+            _bufferOut1 = bufferOut1;
+            _bufferOut2 = bufferOut2;
+        }
+
+        public void Run()
+        {
+            int temp;
+            do
+            {
+                temp = _bufferIn.Take();
+                _bufferOut1.Put(temp);
+                _bufferOut2.Put(temp);
+            } while (temp != -1);
+        }
+    }
+}
--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -11,25 +11,41 @@ namespace ProducerConsumer
     {
         static void Main(string[] args)
         {
-            BoundedBuffer buffer = new BoundedBuffer(4);
-            BoundedBuffer buffer2 = new BoundedBuffer(4); //Brugt til MiddleMan
+            BoundedBuffer buffer = new BoundedBuffer(4); //Brugt til Termination
+            BoundedBuffer buffer2 = new BoundedBuffer(4); //Brugt til MiddleMan + Duplicator
+            BoundedBuffer buffer3 = new BoundedBuffer(4); //Brugt til Duplicator
 
             Producer prod = new Producer(buffer, 15);
+
+            //Brugt til Termination
             //Consumer cons = new Consumer(buffer);
             //Consumer cons2 = new Consumer(buffer);
+
+            //Brugt til MiddleMan
             MiddleMan mm = new MiddleMan(buffer, buffer2); //Tager fra buffer og indsætter i buffer2
             Consumer consMm = new Consumer(buffer2);
 
+            //Brugt til Duplicator
+            Duplicator duplicator = new Duplicator(buffer, buffer2, buffer3); //Tager fra buffer, og sætter værdien ind i både buffer2 & buffer3
+            Consumer consDup = new Consumer(buffer2);
+            Consumer consDup2 = new Consumer(buffer3);
+
             //Brug Parallel.Invoke til at køre dem
-           Parallel.Invoke(
-                () => prod.Run(), 
-                //() => cons.Run(),
-                //() => cons2.Run()
-                () => mm.Run(),
-                () => consMm.Run()
-                );
+            Parallel.Invoke(
+                 () => prod.Run(), //<- Brugt til Termination, MiddleMan, Duplicator
+
+                //() => cons.Run(), //<- Brugt til Termination
+                //() => cons2.Run() //<- Brugt til Termination
+
+                 //() => mm.Run(), //<- Brugt til MiddleMan
+                 //() => consMm.Run() //<- Brugt til MiddleMan
+
+                 () => duplicator.Run(), //<- Brugt til Duplicator
+                 () => consDup.Run(), //<- Brugt til Duplicator
+                 () => consDup2.Run() //<- Brugt til Duplicator
+                 );
             Console.WriteLine("Done!");
-           Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
